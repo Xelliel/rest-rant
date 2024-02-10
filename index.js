6188a6125592b4e152ab1cc2e362d121292ebf1f
Xelliel <malkomguile@yahoo.com>
2024-02-10 09:11:56 -0800

    Adding home and 404 places

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 // Load environment variables from .env file
 require('dotenv').config();
 
+// Set up JSX view engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // Define routes
 app.use('/places', require('./controllers/places'));
 
 // Define a route for the home page
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('Home');
 });
 
-
+// Define a 404 route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>');
+    res.render('error404')
 });
 
 // Start the server
