1bcb1988aa17d728e9e35100efe420796ef37163
Xelliel <malkomguile@yahoo.com>
2024-02-16 21:31:26 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 
 
 
-module.exports = Def
+
 
 
 function Def (html) {
@@ -12,9 +12,23 @@ function Def (html) {
                 <title>Title</title>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home<</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
     )
   }
   
+  module.exports = Def
