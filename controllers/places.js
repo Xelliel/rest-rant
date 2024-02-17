11f0df42abe9c309a76f0ea7ca121523ed47cfde
Xelliel <malkomguile@yahoo.com>
2024-02-16 22:47:38 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const places = require('../models/places.js')
 
+
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+
+  }
+})
+
+
+
 router.get('/', (req, res) => {
     res.send('GET /places/index');
 }); //
@@ -8,6 +26,12 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+
+  router.get('/:id', (req, res) => {
+    res.render('places/show')
+  })
+  
+  
   
  
 
